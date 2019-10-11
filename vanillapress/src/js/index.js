38b2d591e0ebec285ffd30a29e7f380c02c42ac2
Christopher Falzone <cfalzone@aquent.com>
2019-10-11 14:20:43 -0400

    Add a basic layout to the VanillaPress project and build

---
+++ vanillapress/src/js/index.js
@@ -0,0 +1,7 @@
+
+// Get the app Element
+const app = document.querySelector('#app');
+const sidebar = document.querySelector('#sidebar');
+
+app.insertAdjacentHTML('beforeEnd', `<h2>Hello World!</h2>`);
+sidebar.insertAdjacentHTML('beforeEnd', `<h3>Side Bar</h3>`);
