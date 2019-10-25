01601e956f48fdc7d46d8a28661e8f40601e122b
Christopher Falzone <cfalzone@aquent.com>
2019-10-25 15:06:26 -0400

    Listing content from state

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Header from './components/Header';
+import Posts from './components/Posts';
+
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "Hello React",
+        content: "Lorem."
+      },
+      {
+        id: 2,
+        title: "Hello Project",
+        content: "Tothe."
+      },
+      {
+        id: 3,
+        title: "Hello Blog",
+        content: "Ipsum."
+      }
+    ]
+  };
+
+  render = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Posts posts={this.state.posts} />
     </div>
   );
 }
